33957b70a1e5997f52b29af81919b045e3003ab6
Blithe-Chiang <cn18750441251@126.com>
2021-09-03 10:29:51 +0800

    cofig: vite alias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import alias from '@rollup/plugin-alias'
+import path from 'path'
+
+const projectRootDir = path.resolve(__dirname)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [alias(), vue()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(projectRootDir, 'src') },
+      { find: 'components', replacement: path.resolve(projectRootDir, 'src/components') },
+    ]
+  }
 })
